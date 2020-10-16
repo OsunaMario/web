2b3cf29687b4883664d0a261d9ed7db66330e4f3
Mario Osuna <71477284+OsunaMario@users.noreply.github.com>
2020-10-15 20:59:36 -0600

    JS - Ejercicio 1
    Hola mundo en JS

---
+++ JS/1/codigo.js
@@ -0,0 +1,26 @@
+/*=====================================
+=            Instrucciones            =
+Modificar el primer script para que:
+
+1.- Todo el código JavaScript se encuentre en un archivo externo llamado 
+codigo.js y el script siga funcionando de la misma manera.
+2.- Después del primer mensaje, se debe mostrar otro mensaje que diga "Soy 
+el primer script“.
+3.- Añadir algunos comentarios que expliquen el funcionamiento del código.
+4.- Añadir en la página HTML un mensaje de aviso para los navegadores que 
+no tengan activado el soporte de JavaScript.
+=====================================*/
+
+/*=====  End of Instrucciones  ======*/
+
+/* Creación de variable de saludo "Hola mundo"*/
+var saludoHola = "Hola mundo";
+
+/* Creación de variable con aviso "Soy el primer script*/
+var primerScript = "Soy el primer Script";
+
+/* Muestra el mensaje con varaible de saludo*/
+alert(saludoHola);
+
+/* Muestra el mensaje con varaible de aviso*/
+alert(primerScript);
