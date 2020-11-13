cfd090856c2cd42f0b0709ed082ece9c79140e07
Mario Osuna <71477284+OsunaMario@users.noreply.github.com>
2020-11-12 22:02:34 -0700

    PHP - Ejercicio 1
    Hola mundo, iniciando con php

---
+++ PHP/Pruebas/index.php
@@ -0,0 +1,17 @@
+<?php 
+	$name = "Mario";
+	$Name = "Tío Maito";
+	$lastname = "Osuna";
+	$fullname = $name." ".$lastname;
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Site</title>
+</head>
+<body>
+	<h1>
+		Hola mi nombre es <?= $fullname ?>
+	</h1>
+</body>
+</html>
