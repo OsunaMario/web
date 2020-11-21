9b7b862ffe6f9fe8a9f24f6a32cf5b62b0a303d4
Mario Osuna <71477284+OsunaMario@users.noreply.github.com>
2020-11-20 22:49:38 -0700

    PHP - Ejercicio 4
    Session, update and delete elements in a form

--- PHP/PruebaConexion/app/connectionController.php
+++ PHP/PruebaConexion/app/connectionController.php
@@ -3,7 +3,7 @@
 	define("HOST", "localhost");
 	define("USER", "root");
 	define("PSWD", "");
-	define("DBNM", "pruebaweb");
+	define("DBNM", "netflix");
 
 	function connect(){
 		$conn = new mysqli(HOST,USER,PSWD,DBNM);
--- PHP/PruebaConexion/categories/index.php
+++ PHP/PruebaConexion/categories/index.php
@@ -1,92 +1,238 @@
-<?php
+<?php 
 
 	include "../app/categoryController.php";
 	$categoryController = new CategoryController();
+
 	$categories = $categoryController->get();
 
-	echo json_encode($categories);
-
+	#echo json_encode($categories);
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Categories</title>
+	<title>
+		Categories
+	</title>
 	<style type="text/css">
 		table, th, td {
 			border: 1px solid black;
 			border-collapse: collapse;
 		}
-
-
-
+		#updateForm{
+			display: none;
+		}
 	</style>
 </head>
 <body>
 	<div>
+
 		<h1>
 			Categories
-		</h1>
+		</h1> 
+
+		<?php 
+
+		if (isset($_SESSION) && isset($_SESSION['error'])) {
+
+			echo "<h3> Error: ".$_SESSION['error']."</h3>";
+			unset($_SESSION['error']);
+
+		}
+
+		?> 
+
+		<?php if (isset($_SESSION) && isset($_SESSION['error']) ): ?>
+		<h3>
+			Error: <?= $_SESSION['error'] ?>
+		</h3>
+		<?php unset($_SESSION['error']); ?>
+		<?php endif ?>
+
+		<?php if (isset($_SESSION) && isset($_SESSION['success']) ): ?>
+		<h3>
+			Correcto: <?= $_SESSION['success'] ?>
+		</h3>
+		<?php unset($_SESSION['success']); ?>
+		<?php endif ?>
+
+		<table>
+			<thead>
+				<th>
+					#
+				</th>
+				<th>
+					Name
+				</th>
+				<th>
+					Description
+				</th>
+				<th>
+					Actions
+				</th>
+			</thead>
+			<tbody>
+
+				<?php foreach ($categories as $category): ?>
+
+				<tr>
+
+					<td>
+						<?= $category['id'] ?>
+					</td>
+					<td>
+						<?= $category['name'] ?>
+					</td>
+					<td>
+						<?= $category['description'] ?>
+					</td>
+					<td>
+						<button onclick="edit(<?= $category['id'] ?>,'<?= $category['name'] ?>','<?= $category['description'] ?>','<?= $category['status'] ?>')">
+							Edit category
+						</button>
+
+						<button onclick="remove(<?= $category['id'] ?>)" style="background-color: red;color:white;">
+							Delete category
+						</button>
+					</td>
+
+				</tr>
+
+				<?php endforeach ?>
+
+				<?php 
+
+				// foreach ($categories as $category) {
+
+				// 	echo "<tr>
+
+				// 		<td>
+				// 			".$category['id']."
+				// 		</td>
+				// 		<td>
+				// 			".$category['name']."
+				// 		</td>
+				// 		<td>
+				// 			".$category['description']."
+				// 		</td>
+
+				// 		<td>
+				// 			<button onclick='edit(".$category['id'].",\"".$category['name']."\",\"".$category['description']."\",\"".$category['status']."\")' >
+				// 				Edit category
+				// 			</button>
+
+				// 			<button onclick='delete(".$category['id'].")' > Delete category </button>
+
+				// 		</td>
+
+				// 	</tr>";
+
+				// }
+
+
+				?> 
+
+			</tbody>
+		</table>
+
+		<form id="storeForm" action="../app/categoryController.php" method="POST">
+		<form action="../app/categoryController.php" method="POST">
+			<fieldset>
+
+				<legend>
+					Add new Category
+				</legend>
+
+				<label>
+					Name
+				</label>
+				<input type="text"  name="name" placeholder="terror" required=""> 
+				<br>
+
+				<label>
+					Description
+				</label>
+				<textarea placeholder="write here" name="description" rows="5" required=""></textarea>
+				<br>
+
+				<label>
+					Status
+				</label>
+				<select name="status">
+					<option> active </option>
+					<option> inactive </option>
+				</select>
+				<br>
+
+				<button type="submit" >Save Category</button>
+				<input type="hidden" name="action" value="store">
+			</fieldset>
+		</form>
+
+		<form id="updateForm" action="../app/categoryController.php" method="POST">
+			<fieldset>
+
+				<legend>
+					Edit Category
+				</legend>
+
+				<label>
+					Name
+				</label>
+				<input type="text" id="name" name="name" placeholder="terror" required=""> 
+				<br>
+
+				<label>
+					Description
+				</label>
+				<textarea placeholder="write here" id="description" name="description" rows="5" required=""></textarea>
+				<br>
+
+				<label>
+					Status
+				</label>
+				<select id="status" name="status">
+					<option> active </option>
+					<option> inactive </option>
+				</select>
+				<br>
+
+				<button type="submit" >Save Category</button>
+				<input type="hidden" name="action" value="update">
+				<input type="hidden" name="id" id="id">
+
+			</fieldset>
+		</form>
+
+		<form id="destroyForm" action="../app/categoryController.php" method="POST">
+
+			<input type="hidden" name="action" value="destroy">
+			<input type="hidden" name="id" id="id_destroy">
+
+		</form>
+
 	</div>
-<table>
-	<thead>
-		<th>
-			#
-		</th>
-		<th>
-			Name
-		</th>
-		<th>
-			Description
-		</th>
-	</thead>
-	<tbody>
-	<?php
-	foreach ($categories as $category) {
-		echo "<tr>
-				<td>
-					".$category['id']."
-				</td>
-				<td>
-					".$category['name']."
-				</td>
-				<td>
-					".$category['description']."
-				</td>
-			</tr>";
-	}
+	<script type="text/javascript">
+		function edit(id,name,description,status)
+		{	
+			document.getElementById('storeForm').style.display="none";
+			document.getElementById('updateForm').style.display="block";
 
-	?>
-</tbody>
-</table>
+			document.getElementById('name').value = name;
+			document.getElementById('description').value = description;
+			document.getElementById('status').value = status;
+			document.getElementById('id').value = id;
+		}
 
-<form action="../app/categoryController.php" method="POST">
-	<fieldset>
-		<legend>
-			Add new Category
-		</legend>
-		<label>
-			Name
-		</label>
-		<input type="text" name="name" placeholder="terror" required="">
-		<label>
-			Description
-		</label>
-		<textarea placeholder = "write here" name = "description"></textarea>
-		<br>
+		function remove(id)
+		{
+			var confirm = prompt("Si quiere eliminar el registro, escriba :"+id);
+			if (confirm == id) {
 
-		<label>
-			Status
-		</label>
-		<select name="status">
-			<option> active </option>
-			<option> inactive </option>
-		</select>
-		<br>
+				document.getElementById('id_destroy').value = id;
+				document.getElementById('destroyForm').submit();
 
-		<button type="submit">Save Category</button>
-		<input type="hidden" name="action" value="store">
-	</fieldset>
-</form>
-
+			}
+		}
+	</script>
 </body>
 </html>
